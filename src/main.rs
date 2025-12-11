01f3a04ed44580765bbb7d44c09af98e0590febe
Jacek Czaja <jacek.czaja@intel.com>
2025-12-11 19:43:06 +0100

    - added egui_plot

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
-use macroquad::prelude::*;
+use egui_plot::{Bar, BarChart, Line, Plot, PlotPoints};
+use macroquad::prelude::*; // Import necessary components
 
 #[macroquad::main("egui with macroquad")]
 async fn main() {
     loop {
         clear_background(WHITE);
 
-        // Process keys, mouse etc.
-
         egui_macroquad::ui(|egui_ctx| {
             egui_macroquad::egui::Window::new("egui ❤ macroquad").show(egui_ctx, |ui| {
                 ui.label("Test");
+
+                // Process keys, mouse etc.
+                let points = PlotPoints::from_iter(vec![[0.0, 1.0], [1.0, 3.0], [2.0, 2.0]]);
+                let line = Line::new("linie", points);
+
+                Plot::new("my_plot").show(ui, |plot_ui| {
+                    // Customize axes, legends, etc. here
+                    plot_ui.line(line);
+                });
             });
         });
 
